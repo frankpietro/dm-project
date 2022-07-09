e4bfbb14b34f3819f2620442e3309fc311d49df4
frankpietro <pfrancaviglia@gmail.com>
2022-07-09 11:28:50 +0200

    Purified datasets

--- utility.py
+++ utility.py
@@ -10,15 +10,27 @@ def dropextr(serie):
     return serie.sort_values()
     # [round(len(serie)/20):-round(len(serie)/20)]
 
-def hist(serie, bins, title):
+def hist(df, col, bins, title, perc=0):
+    df = df.sort_values(col)
+    size = df[col].size
+    cut = int(size*perc)
     plt.figure(figsize=(4, 2))
-    sns.histplot(serie, bins=bins)
+    if cut:
+        sns.histplot(df[col][cut:-cut], bins=bins)
+    else:
+        sns.histplot(df[col], bins=bins)
     plt.xticks(rotation=45)
     plt.title(title)
-    return plt.show()
+    plt.show()
+            
 
-def loghist(serie, title):
-    return hist(serie, logbins(serie), title)
+def loghist(df, col, title, perc=0):
+    hist(df, col, logbins(df[col]), title, perc)
 
 def rankings(serie):
     return pd.cut(dropextr(serie), bins = logbins(serie), labels = range(logbins(serie)))
+
+def double_loghist(df1, df2, col, title, perc=0):
+    plt.figure(figsize=(9, 4))
+    loghist(df1, col, title, perc)
+    loghist(df2, col, title, perc)
