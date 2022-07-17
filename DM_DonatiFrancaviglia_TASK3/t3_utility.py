fd11b231df82cc79777a81759f604786bbc9912d
frankpietro <pfrancaviglia@gmail.com>
2022-07-17 16:43:49 +0200

    T3

--- DM_DonatiFrancaviglia_TASK3/t3_utility.py
+++ DM_DonatiFrancaviglia_TASK3/t3_utility.py
@@ -2,25 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
-def logbins(df):
-    return np.ceil(1 + (10 / 3) * np.log10(len(df))).astype(int)
-
-
-def hist(df, col, bins, title, perc=0):
-    df = df.sort_values(col)
-    size = df[col].size
-    cut = int(size*perc)
-    plt.figure(figsize=(4, 2))
-    if cut:
-        sns.histplot(df[col][cut:-cut], bins=bins)
+def scale_df(df, method):
+    if method == 1:
+        array = StandardScaler().fit_transform(df)
     else:
-        sns.histplot(df[col], bins=bins)
-    plt.xticks(rotation=45)
-    plt.title(title)
-    plt.show()
-            
+        array = MinMaxScaler().fit_transform(df)
+    return array, pd.DataFrame(data=array, index=df.index, columns=df.columns)
 
-def loghist(df, col, title, perc=0):
-    hist(df, col, logbins(df[col]), title, perc)
+
+def rankings(series, type, bins):
+    if type == 0:  # natural binning
+        return pd.cut(series.sort_values(), bins=bins, labels=range(bins))
+    elif type == 1:  # frequency binning
+        return pd.qcut(series.sort_values(), q=bins, labels=range(bins))
