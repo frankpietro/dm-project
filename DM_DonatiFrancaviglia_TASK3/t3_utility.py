3a75b80af4b41355de252229fed21f1c9d7c6347
Hdjdox <giorgio1099@gmail.com>
2022-07-20 19:14:09 +0200

    SVC both

--- DM_DonatiFrancaviglia_TASK3/t3_utility.py
+++ DM_DonatiFrancaviglia_TASK3/t3_utility.py
@@ -8,21 +8,6 @@ import pickle
 import itertools
 from sklearn.model_selection import RandomizedSearchCV
 
-
-def scale_df(df, method):
-    if method == 1:
-        array = StandardScaler().fit_transform(df)
-    else:
-        array = MinMaxScaler().fit_transform(df)
-    return array, pd.DataFrame(data=array, index=df.index, columns=df.columns)
-
-
-def rankings(series, type, bins):
-    if type == 0:  # natural binning
-        return pd.cut(series.sort_values(), bins=bins, labels=range(bins))
-    elif type == 1:  # frequency binning
-        return pd.qcut(series.sort_values(), q=bins, labels=range(bins))
-
 def cross_validation(model, x, y, n_splits):
     """Return validation scores across the k folds of cross-validation."""
     skf = StratifiedKFold(n_splits=n_splits, random_state=0, shuffle=True)
@@ -36,23 +21,3 @@ def cross_validation_summary(model, x, y):
     """Returns validation accuracy score of model (mean and std over all the splits)."""
     val_score = cross_validation(model, x, y, 5)
     return val_score.mean(), val_score.std()
-
-
-def randomized_cv(model, x, y, param_d, n_iter=100):
-    """Perform hyper-parameters grid search and return best configuration."""
-    rf_random = RandomizedSearchCV(
-        estimator = model,
-        param_distributions = param_d,
-        n_iter = n_iter,
-        cv = 5,
-        n_jobs = -1
-    )
-
-    # Run grid search
-    rf_random.fit(x, y)
-    mean_acc, std_acc = cross_validation_summary(rf_random.best_estimator_, x, y)
-
-    # Print configuration  and stats about best model
-    print(f'{rf_random.best_estimator_}\n mean acc: {mean_acc:.3f}\n std_acc: {std_acc:.3f}')
-
-    return rf_random.best_estimator_, rf_random.cv_results_
